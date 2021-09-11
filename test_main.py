652d3d00005249067ff85ceb1d35f670031d9cb4
EC2 Default User <ec2-user@ip-172-31-7-121.us-east-2.compute.internal>
2021-09-11 01:12:23 +0000

    added new test case

--- test_main.py
+++ test_main.py
@@ -2,14 +2,30 @@ from fastapi.testclient import TestClient
 from fastapi import FastAPI
 from main import app
 import pytest
+import requests
+from bs4 import BeautifulSoup
 
 client = TestClient(app)
 
 def test_main():
+    # test root function
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Proceed to IP/docs to enter testing values"}
     
+    # test get recipe function
     response = client.get("/get_recipe/milk")
+    result = {}
+    url = requests.get("https://www.allrecipes.com/search/results/?search=milk")
+    soup = BeautifulSoup(url.content, features="html.parser")
+    top10_rep = soup.find_all("a", class_ = "card__titleLink manual-link-behavior")[:10]
+    count = 1
+    
+    for link in top10_rep:
+        n = "Recipe %d" % count
+        count += 1
+        result[n] = link['href']
+    
     assert response.status_code == 200
     assert response.json() != {}
+    assert response.json() == result
